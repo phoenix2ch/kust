59aa92408325eac2a35b438125d4f29939fc9f6f
phoenix2ch <benoit.zuckschwerdt@gmail.com>
2013-09-09 22:15:24 +0200

    Create App.php

---
+++ App.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Application MVC system class
+ *
+ * Author:	Benoit Zuckschwerdt
+ * Date:	7 August 2012
+ *
+ *
+ */
+
+
+# Security access
+if( !defined( "IN_SITE" ) ){
+	header("HTTP/1.0 403 Forbidden");
+	die();
+}
+
+
+class Kust_App
+{
+	private static $frameworkName = 'DubGigaFisherMaster Framework version 0.1';
+
+	public $uri;
+
+
+	/**
+	 * Constructor
+	 * @param array $uri
+	 */
+	public function __construct( $uri = null ) {
+		$this->uri = $uri;
+
+		$this->loadController( $uri['controller'] );
+	}
+
+
+	/**
+	 * Load controller
+	 * @param string $class
+	 */
+	public function loadController( $class )
+	{
+		$file = "app/controllers/".$this->uri['controller'].".controller.php";
+
+		if(!file_exists($file)) die( "controller not found at $file" );
+
+		require_once($file);
+
+		$controller = new $class();
+
+		if( method_exists( $controller, $this->uri['method'] ) ){
+			$controller->{$this->uri['method']}( $this->uri['var'] );
+		}
+		else {
+			$controller->index();
+		}
+	}
+
+
+	/**
+	 * Get Framework name
+	 * @return string
+	 */
+	public function getFrameworkName() {
+		return self::$frameworkName;
+	}
+}
+?>
