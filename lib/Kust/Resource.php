1b5e9bf59efcdc975fb16a9739694647c6e6de37
phoenix2ch <benoit.zuckschwerdt@gmail.com>
2013-09-10 18:06:04 +0200

    Create Resource.php

---
+++ lib/Kust/Resource.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * MYSQL Resource class
+ * Kust/Resource.php
+ *
+ * --------------------
+ * Author:		Benoît Zuckschwerdt @ 09.11.2010
+ * --------------------
+ *
+ * Resource mysql.
+ *
+ **/
+
+class Kust_resource{
+	private $res;
+	
+	function __construct($res) {
+		$this->res	= $res;
+	}
+	
+	function fetchAll() {
+		while($aLineData = mysql_fetch_assoc($this->res))
+			$aData[] = $aLineData;
+			
+		return $aData;
+	}
+	
+	function fetch() {
+		return mysql_fetch_assoc($this->res);
+	}
+	
+	function count() {
+		return mysql_num_rows($this->res);
+	}
+}
+
+?>
